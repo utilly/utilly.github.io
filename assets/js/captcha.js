8dec3ce142a6ecfd82181800c6e916bd6aa5839b
Nick Williamson <koodeau@gmail.com>
2018-10-30 17:35:57 +0200

    preview update (pre-release)

--- assets/js/captcha.js
+++ assets/js/captcha.js
@@ -3,6 +3,7 @@ function Captcha(){
         'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', 
             '0','1','2','3','4','5','6','7','8','9','!','@','#','$','&','*','-','+','=','.');
     var i;
+    var res;
     for (i=0;i<7;i++){
         var a = alpha[Math.floor(Math.random() * alpha.length)];
         var b = alpha[Math.floor(Math.random() * alpha.length)];
@@ -14,7 +15,7 @@ function Captcha(){
         var h = alpha[Math.floor(Math.random() * alpha.length)];
 
                      }
-        var code = a + '' + b + '' + '' + c + '' + d + '' + e + ''+ f + '' + g + '';
+        var code = a + '' + b + '' + '' + c + '' + d + '' + e + ''+ f + '' + g + ''+ h;
         document.getElementById("mainCaptcha").innerHTML = code
         document.getElementById("mainCaptcha").value = code
       }
@@ -22,8 +23,10 @@ function ValidCaptcha(){
     var string1 = removeSpaces(document.getElementById('mainCaptcha').value);
     var string2 =         removeSpaces(document.getElementById('txtInput').value);
     if (string1 == string2){
-           return true;
-    }else{        
+        var res = true;   
+        return true;
+    }else{ 
+        var res = false;       
          return false;
          }
 }
